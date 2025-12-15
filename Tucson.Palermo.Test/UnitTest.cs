830132abffdaf5730d0ceec913f0fe19fd00cbe1
Nicolas Dominguez <nicolas.dominguez.it@outlook.com>
2025-12-15 16:01:15 -0300

    Se agregan 2 test unitarios

--- Tucson.Palermo.Test/UnitTest.cs
+++ Tucson.Palermo.Test/UnitTest.cs
@@ -7,6 +7,10 @@ namespace Tucson.Palermo.Test
 {
     public class UnitTest
     {
+        /* 
+           Las pruebas unitarias deben ejecutarse en un contexto aislado, sin producir efectos colaterales ni modificar el estado persistente de las reservas.
+        */
+
         [Fact]
         public void FiltrarReservas_DevuelveResultados()
         {
@@ -40,6 +44,54 @@ namespace Tucson.Palermo.Test
             // Assert
             Assert.True(resultado.Count >= 0);
         }
+
+        [Fact]
+        public void ValidarReserva_NoDevuelveErrores()
+        {
+            var reserva = new Reserva
+            {
+                Fecha = DateTime.Now.AddDays(1), // fecha futura
+                HoraInicio = new TimeSpan(19, 0, 0),
+                HoraFin = new TimeSpan(23, 0, 0)
+            };
+
+            int horaInicio = 19;
+            int minutoInicio = 0;
+            int horaFin = 23;
+            int minutoFin = 30;
+
+            // Act
+            var context = GetRealDbContext();
+            var service = new ReservaService(context);
+            var errores = service.ValidarReserva(reserva, horaInicio, minutoInicio, horaFin, minutoFin);
+
+            // Assert
+            Assert.Empty(errores);
+        }
+        [Fact]
+        public void ValidarReserva_DevuelveErrores()
+        {
+            var reserva = new Reserva
+            {
+                Fecha = DateTime.Now.AddDays(-1), // fecha pasada
+                HoraInicio = new TimeSpan(19, 0, 0),
+                HoraFin = new TimeSpan(23, 0, 0)
+            };
+
+            int horaInicio = 19;
+            int minutoInicio = 0;
+            int horaFin = 23;
+            int minutoFin = 30;
+
+            // Act
+            var context = GetRealDbContext();
+            var service = new ReservaService(context);
+            var errores = service.ValidarReserva(reserva, horaInicio, minutoInicio, horaFin, minutoFin);
+
+            // Assert
+            Assert.Contains(errores, e => e.campo == "Fecha");
+        }
+
 
         private AppDbContext GetRealDbContext()
         {
